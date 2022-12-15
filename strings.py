7a4f575b723f96d5e94ae1b566548884249a67c2
jwagner9307 <116571986+jwagner9307@users.noreply.github.com>
2022-12-15 18:05:51 +0000

    a# Please enter the commit message for your changes. Lines starting

---
+++ strings.py
@@ -0,0 +1,19 @@
+phrase = "Sincerly\n\"Jack Wagner\""
+          
+print(phrase)
+#capatalize all the text
+print(phrase.upper())
+#boolean 
+print(phrase.upper().isupper())
+
+#length
+print(len(phrase))
+
+#individual character
+print(phrase[0])
+
+#index
+print(phrase.index("Wagner"))
+
+#replace text
+print(phrase.replace("Jack", "Norman"))
---
+++ working-with-numbers.py
@@ -0,0 +1,43 @@
+#print a number
+print(1)
+
+#print a decimal
+print(1.0)
+
+#print a negative number
+print(-1)
+
+#print arithmatics
+print(1+1)
+
+#print order of operation
+print((2+2)*5)
+
+#Print the remainder, or "mod(%)"
+print(10%3)
+
+#print numbers along side strings
+my_num = 5
+print(str(my_num) + " my favorite number")
+
+#functions
+#absolute value
+print(abs(my_num))
+
+#power (x^y)
+print(pow(2, 2))
+
+#max function
+#determine the biggest number
+print(max(4, 5, 6))
+
+#round function
+print(round(4.8))
+
+#import python math
+from math import *
+
+#floor method(always round down)
+print (floor(5.999))
+#ceil (always round up)
+print(ceil(4.1))
