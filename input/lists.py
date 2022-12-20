00c75bab1a16a89b6ae60da3c6770972c1c85de2
jwagner9307 <116571986+jwagner9307@users.noreply.github.com>
2022-12-20 14:12:41 +0000

    commit

--- input/lists.py
+++ input/lists.py
@@ -1,1 +1,13 @@
-animals = ["Cow", "pig", "Cat", "dog"]
+
+animals = ["Cow",   "Pig",  "Dog",  "Cat"]
+#          0        1       2       3
+#each element in the list has an index value 
+print(animals)
+print(animals[0])
+print(animals[-1])
+
+#grab elements 1 and every element after that
+print(animals[1:])
+
+#grab elements betweet two indexes
+print(animals[1:3])
