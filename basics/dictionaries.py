a803c687908ce709056c0559bf1f002747b1715d
jwagner9307 <116571986+jwagner9307@users.noreply.github.com>
2023-01-17 16:53:56 +0000

    commit

---
+++ basics/dictionaries.py
@@ -0,0 +1,16 @@
+#the words in a dictoinary are teh key
+#the defonition is the value
+
+monthConversions = {
+    "Jan": "January",
+    "Feb": "Febuary"
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Nov": "November",
+    "Dec": "December",
+}
